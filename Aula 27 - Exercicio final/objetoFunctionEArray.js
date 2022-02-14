371cccc4aff15b95e8d4e5ac8ead34b5d256cf29
Luidy <99658182+Luidyc@users.noreply.github.com>
2022-02-14 09:45:56 -0300

    Add files via upload

---
+++ Aula 27 - Exercicio final/objetoFunctionEArray.js
@@ -0,0 +1,30 @@
+function pessoas () {
+const form = document.querySelector('.form');
+const resultado = document.querySelector('.resultado');
+const pessoas = []; 
+
+function recebeEventoForm (evento) {
+    evento.preventDefault();
+    
+    const nome = form.querySelector('.nome');
+    const sobrenome = form.querySelector('.sobrenome');
+    const peso = form.querySelector('.peso');
+    const altura = form.querySelector('.altura');
+    
+    pessoas.push({
+        nome: nome.value,
+        sobrenome: sobrenome.value,
+        peso: peso.value,
+        altura: altura.value
+      });
+    console.log(pessoas);
+
+    resultado.innerHTML += `<p> Seu nome completo é: ${nome.value} ${sobrenome.value} `+
+    `seu peso e altura são: ${peso.value} kg, ${altura.value} cm </p>`;
+}
+
+
+
+form.addEventListener('submit', recebeEventoForm);
+}
+pessoas();
